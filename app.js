34c1ce520209a50ad85000f24427acb87d7f837e
Chris Bentley <chrisrbentley@pm.me>
2023-08-29 12:40:59 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+import createError from 'https';
+import cookieParser from 'cookie-parser';
+import express from 'express';
+const app = express();
+
+import indexRouter from './routes/index.js';
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+
+app.use((req, res, next) => {
+	next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+app.listen(3000, () => {
+	console.log('App listening on port 3000');
+});
