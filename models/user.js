b032e5fdb080d4cddee24d14053532049f1fa71e
Chris Bentley <chrisrbentley@pm.me>
2023-09-18 14:28:27 -0400

    Removed member field from user model

--- models/user.js
+++ models/user.js
@@ -5,7 +5,6 @@ const Schema = mongoose.Schema;
 const UserSchema = new Schema({
 	username: { type: String, required: true, minLength: 1, maxLength: 20 },
 	password: { type: String, required: true, minLength: 4, maxLength: 80 },
-	member: { type: Boolean, required: true },
 });
 
 export default mongoose.model('User', UserSchema);
